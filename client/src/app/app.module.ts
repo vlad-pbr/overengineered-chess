c45f59af9a368757060e83349b3d360cdc12d358
Vlad <vlad.pbr@gmail.com>
2022-11-01 15:08:16 +0200

    feat(client): init client skeleton

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { GameComponent } from './game/game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    GameComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'menu', component: MenuComponent},
+      {path: 'game', component: GameComponent},
+      {path: '', redirectTo: '/menu', pathMatch: 'full'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
