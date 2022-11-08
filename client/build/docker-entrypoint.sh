b5851d1d614251c6ac6c4feee3bdb98c2474a685
Vlad <vlad.pbr@gmail.com>
2022-11-08 17:03:00 +0200

    fix(client): not substituting vars in favor of bind mounting

--- client/build/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# substitute parameters
-envsubst < /usr/share/nginx/html/env.json > /tmp/env.json
-mv /tmp/env.json /usr/share/nginx/html/env.json
-
-# run nginx as usual
-exec /docker-entrypoint.sh "$@"
